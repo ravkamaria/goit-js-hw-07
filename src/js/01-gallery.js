75ef970f2c94c51b92c8f8f85e204202ccf639cb
ravkamaria <hevkomaria1@gmail.com>
2023-08-02 02:12:09 +0300

    Task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+gallery.addEventListener("click", handlerClick);
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  let currentImageLink = evt.target.dataset.source;
+  const instance = basicLightbox.create(`<img
+        class="gallery__image"
+        src="${currentImageLink}"
+      />`);
+  instance.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
